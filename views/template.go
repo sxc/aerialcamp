39e9512af6a8ca95d4057f914393426d0affefcb
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-06-20 14:53:49 +0800

    add dynmic faq page

--- views/template.go
+++ views/template.go
@@ -37,7 +37,7 @@ type Template struct {
 
 func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	err := t.HTMLTpl.Execute(w, nil)
+	err := t.HTMLTpl.Execute(w, data)
 	if err != nil {
 		log.Printf("executing template: %v", err)
 		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
