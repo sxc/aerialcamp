3f59cc3a3ceeb712236eb7c998c96fa17afebb1a
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-06-29 11:55:40 +0800

    update or insert session

--- models/session.go
+++ models/session.go
@@ -42,11 +42,25 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 		Token:     token,
 		TokenHash: ss.hash(token),
 	}
-	// Insert to DB
-	row := ss.DB.QueryRow(`INSERT INTO sessions 
-	(user_id, token_hash) 
-	VALUES ($1, $2) 
+	// 1. Query user
+	// 2. if found , update session
+	// 3. if not found , create a new session
+
+	row := ss.DB.QueryRow(`
+    UPDATE sessions
+	SET token_hash = $2
+	WHERE user_id = $1
 	RETURNING id`, session.UserID, session.TokenHash)
+	err = row.Scan(&session.ID)
+	if err == sql.ErrNoRows {
+		// Insert to DB
+		row = ss.DB.QueryRow(`
+		INSERT INTO sessions 
+		(user_id, token_hash) 
+		VALUES ($1, $2) 
+		RETURNING id`, session.UserID, session.TokenHash)
+	}
+
 	err = row.Scan(&session.ID)
 	if err != nil {
 		return nil, fmt.Errorf("create: %w", err)
