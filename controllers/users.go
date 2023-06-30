9bd2de1b1547013d9b18c48d8c461bc2ab4ee985
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-06-30 20:43:20 +0800

    add context

--- controllers/users.go
+++ controllers/users.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"net/http"
 
+	"github.com/sxc/aerialcamp/context"
 	"github.com/sxc/aerialcamp/models"
 
 	"github.com/gorilla/csrf"
@@ -85,21 +86,28 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) CurrentUser(w http.ResponseWriter, r *http.Request) {
-	// tokenCookie, err := r.Cookie(CookieSession)
-	token, err := readCookie(r, CookieSession)
-	if err != nil {
-		fmt.Println(err)
-		// http.Error(w, "Something went wrong.", http.StatusInternalServerError)
-		http.Redirect(w, r, "/signin", http.StatusFound)
-		return
-	}
-	user, err := u.SessionService.User(token)
-	if err != nil {
-		fmt.Println(err)
+	user := context.User(r.Context())
+	if user == nil {
 		http.Redirect(w, r, "/signin", http.StatusFound)
 		return
 	}
 	fmt.Fprintf(w, "Current user : %s\n", user.Email)
+
+	// tokenCookie, err := r.Cookie(CookieSession)
+	// token, err := readCookie(r, CookieSession)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	// http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+	// 	http.Redirect(w, r, "/signin", http.StatusFound)
+	// 	return
+	// }
+	// user, err := u.SessionService.User(token)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	http.Redirect(w, r, "/signin", http.StatusFound)
+	// 	return
+	// }
+	// fmt.Fprintf(w, "Current user : %s\n", user.Email)
 }
 
 func (u Users) PorcessSignOut(w http.ResponseWriter, r *http.Request) {
