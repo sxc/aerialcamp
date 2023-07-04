12a441d830a5d8f20b8b9dd56755ff36e240df48
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-07-04 09:46:50 +0800

    add forgot password template

--- main.go
+++ main.go
@@ -68,6 +68,11 @@ func main() {
 		"signin.gohtml", "tailwind.gohtml",
 	))
 
+	usersC.Templates.ForgotPassword = views.Must(views.ParseFS(
+		templates.FS,
+		"forgot-pw.gohtml", "tailwind.gohtml",
+	))
+
 	umw := controllers.UserMiddleware{
 		SessionService: &sessionService,
 	}
@@ -97,6 +102,8 @@ func main() {
 	r.Get("/signin", usersC.SignIn)
 	r.Post("/signin", usersC.ProcessSignIn)
 	r.Post("/signout", usersC.PorcessSignOut)
+	r.Get("/forgot-pw", usersC.ForgotPassword)
+	r.Post("/forgot-pw", usersC.ProcessForgotPassword)
 	// r.Get("/users/me", usersC.CurrentUser)
 	r.Route("/users/me", func(r chi.Router) {
 		r.Use(umw.RequireUser)
