0114b5ff31a30791eeeae5362b9dd1af8c802992
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-10-05 11:32:00 +0800

    add import

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "/templates/**/*.{gohtml,html}"
+  ],
+
+  important: true,
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
